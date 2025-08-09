d61133caaca074219fc04495476f337e217cdcdc
Chris <christophercrossett23@gmail.com>
2025-08-08 21:26:31 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,15 @@
 // Replace the below config with your Firebase project's config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "const firebaseConfig = {
+  apiKey: "AIzaSyA... (your actual keys)",
+  authDomain: "unlocking-your-future.firebaseapp.com",
+  projectId: "unlocking-your-future",
+  storageBucket: "unlocking-your-future.appspot.com",
+  messagingSenderId: "138835217958",
+  appId: "1:138835217958:web:add1725c6b9c1c579338867",
+  measurementId: "G-81F25RT1RI"
+};
+",
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_STORAGE_BUCKET",
