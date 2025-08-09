ec0721554f545f3f6f9832166c31454ed3318821
Chris <christophercrossett23@gmail.com>
2025-08-08 21:17:00 -0400

    Create Admin.js

---
+++ src/pages/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Admin() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Admin Panel</h2>
+      <p>This page will be used to add/edit questions and manage content.</p>
+      <p>(Coming soon!)</p>
+    </div>
+  );
+}
+
+export default Admin;
