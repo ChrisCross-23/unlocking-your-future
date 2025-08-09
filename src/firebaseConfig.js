78f941435e9d414af94757d6ffd805110a39bdc3
Chris <christophercrossett23@gmail.com>
2025-08-08 21:04:11 -0400

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Replace the below config with your Firebase project's config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+export default firebaseConfig;
